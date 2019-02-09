7f86dab247c61541ad4e2d60959f27c12e55af65
yutaro1031 <yutaro50@gmail.com>
2019-02-09 15:24:01 +0900

    ユーザー登録機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def me
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
