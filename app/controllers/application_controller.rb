89d7c9139068461fec503f4a04778564076186d8
yutaro1031 <yutaro50@gmail.com>
2019-02-09 14:31:46 +0900

    ヘッダーベース作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :current_user
+
   def render_404
     respond_to do |format|
       format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
+  end
+
+  private
+
+  def current_user
+    return unless session[:user_id]
+    @current_user = User.find_by(id: session[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'articles#index' ログイン状態でリダイレクトを行うようにする
+  root 'articles#index' #ログイン状態でリダイレクトを行うようにする
   resources :articles
+  resources :users, only: %i[new create]
+
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
