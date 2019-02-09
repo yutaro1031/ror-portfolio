ba9ad07bbe2600163b144dd74d552ba3097e3771
yutaro1031 <yutaro50@gmail.com>
2019-02-09 17:41:45 +0900

    ログイン・ログアウト実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(name: params[:post][:name])
+    if user && user.authenticate(params[:post][:password])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      redirect_to articles_path
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'articles#index' #ログイン状態でリダイレクトを行うようにする
+  root 'home#index' #ログイン状態でリダイレクトを行うようにする
   resources :articles
   resources :users, only: %i[new create]
 
