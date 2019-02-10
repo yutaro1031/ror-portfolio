9e6c96037de4227a7e0d0a8e84477ac5f42d646b
yutaro1031 <yutaro50@gmail.com>
2019-02-10 11:52:47 +0900

    記事作成機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :current_user
 
   def render_404
     respond_to do |format|
@@ -10,10 +9,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
-
-  def current_user
-    return unless session[:user_id]
-    @current_user = User.find_by(id: session[:user_id])
-  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,36 @@
 class ArticlesController < ApplicationController
   # 各アクションの実行前にset_target_articleが実行されるように指定
   before_action :set_target_article, only: %i[show edit update destroy]
-
+  
   def index # 記事一覧を表示
     @articles = Article.where(del_flg: FALSE)
     @articles = @articles.page(params[:page])
   end
 
   def show # 記事詳細画面
+  end
 
+  def new
+    @article = Article.new(flash[:article])
+  end
+
+  def create
+    article = Article.new(article_params)
+    if article.save
+      flash[:notice] = "「#{article.title}」の記事を作成しました"
+      redirect_to articles_path
+    else
+      redirect_to new_article_path, flash: {
+          article: article,
+          error_messages: ["記事の作成に失敗しました"]
+      }
+    end
   end
 
   private
 
   def article_params
-    # 余分なparamを排除する
+    params.require(:article).permit(:user_id, :title, :text, tag_ids: [])
   end
 
   def set_target_article
