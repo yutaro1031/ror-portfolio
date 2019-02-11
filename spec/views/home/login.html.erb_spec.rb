d89e338421d0c2f7719d345ff6339f3aad07a8a3
yutaro1031 <yutaro50@gmail.com>
2019-02-11 15:21:25 +0900

    home/loginテスト追加

--- spec/views/home/login.html.erb_spec.rb
+++ spec/views/home/login.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "home/login", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "flashが表示されるか" do
+    flash[:error_messages] = ["test"]
+
+    render
+    expect(rendered).to have_selector 'li', text: "test"
+  end
 end
