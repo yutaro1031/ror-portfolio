628eebe4e6080e1b43fbab09ab89fec76178c5a6
yutaro1031 <yutaro50@gmail.com>
2019-02-16 12:14:26 +0900

    controller/sessions#createテスト追加

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -22,18 +22,20 @@ RSpec.describe ArticlesController, type: :controller do
     before do
       user = FactoryBot.create(:user)
       @article = FactoryBot.create(:article, user: user)
-      get :show, params: { id: @article }
     end
 
     it "正常なレスポンスか" do
+      get :show, params: { id: @article }
       expect(response).to be_successful
     end
 
     it "200レスポンスが返ってくるか" do
+      get :show, params: { id: @article }
       expect(response).to have_http_status(:success)
     end
 
     it ":indexテンプレートをレンダリングしているか" do
+      get :show, params: { id: @article }
       expect(response).to render_template :show
     end
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "POST #create" do
+    before do
+      @user = FactoryBot.create(:user)
+    end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    it "正しい場合のリダイレクト先が正しいか" do
+      post :create, params: { post: { name: @user.name, password: @user.password } }
+      expect(response).to redirect_to articles_path
+    end
+
+    it "nameを間違った場合のリダイレクト先が正しいか" do
+      post :create, params: { post: { name: "hogehoge", password: @user.password } }
+      expect(response).to redirect_to authenticate_path
+    end
+
+    it "passwordを間違った場合のリダイレクト先が正しいか" do
+      post :create, params: { post: { name: @user.name, password: "hogehoge" } }
+      expect(response).to redirect_to authenticate_path
     end
   end
-
 end
