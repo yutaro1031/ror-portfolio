c1a40d7d66788734e305dcb150f55b1f0bdb876f
yutaro1031 <yutaro50@gmail.com>
2019-02-16 12:46:14 +0900

    controller/user#createテスト追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+
+    it "正しい場合のリダイレクト先が正しいか" do
+      post :create, params: { user: FactoryBot.attributes_for(:user) }
+      expect(response).to redirect_to articles_path
+    end
+
+    it "nameのバリデーションが効いているか" do
+      post :create, params: { user: FactoryBot.attributes_for(:user, name: "asjdfasljfiejfa;sfna;oejfoai") }
+      expect(response).to redirect_to new_user_path
+    end
+
+    it "passwordのバリデーションが効いているか" do
+      post :create, params: { user: FactoryBot.attributes_for(:user, password: "hogehoge") }
+      expect(response).to redirect_to new_user_path
     end
   end
-
 end
