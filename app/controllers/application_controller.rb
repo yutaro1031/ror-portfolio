0deffc216fab47239a9b91a9c27e2b9ce2377f9b
yutaro1031 <yutaro50@gmail.com>
2019-02-17 13:45:27 +0900

    パス調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ストロングパラメータの設定
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :del_flg, :remember_me])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :del_flg, :remember_me])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password])
+  end
+
+  # ログイン・ログアウト時のリダイレクト先
+  def after_sign_in_path_for(resource)
+    article_path
+  end
+
+  def after_sign_out_path_for(resource)
+    article_path
   end
 
   def render_404
