0716e4d44d454ab290d9037d5d0ffdaac49db636
yutaro1031 <yutaro50@gmail.com>
2019-02-21 16:01:17 +0900

    mypegeをdeviceに組み込む

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def my_page
+    render_404 unless current_user.admin_flg
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: "users/registrations"
+  }
+
   resources :articles
 
-  get 'mypage', to: 'users#me'
+  #get 'mypage', to: 'users#me'
   get 'admin', to: 'admin#index'
   get 'regist_admin_user', to: 'admin#sign_up'
+
+  devise_scope :user do
+    get 'my_page', to: 'users/registrations#my_page'
+  end
 end
