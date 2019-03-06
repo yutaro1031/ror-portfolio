f882f0486cea820ebacbf8b7cdda762f9d0d4565
yutaro1031 <yutaro50@gmail.com>
2019-03-06 09:05:54 +0900

    controller実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,19 +36,21 @@ class ArticlesController < ApplicationController
     case article_params[:update_type]
 
     when 'tmp' # 下書き保存
-      if @article.update_attributes(tmp_article_params)
-        render json: {result: "ok"}
-      else
-        render json: {result: "error"}
-      end
+      update_article(tmp_article_params)
 
-    when 'publish' # 公開設定
+    when 'publish' # 公開設定(タグもそこで行う)
+      update_article(publish_article_params)
 
     when 'eyecatch' # アイキャッチ画像
+      if article_params[:remove_tmp_eyecatch_flg]
+        remove_eyecatch(true)
+      elsif article_params[:remove_eyecatch_flg]
+        remove_eyecatch(false)
+      else
+        render json: {result: "error: nothing remove_flg"}
+      end
 
-    when 'tags' # タグ
-
-    else render json: {result: "nothing change_type"}
+    else render json: {result: "error: nothing change_type"}
     end
   end
 
@@ -70,6 +72,7 @@ class ArticlesController < ApplicationController
                                     :tmp_eyecatch,
                                     :remove_eyecatch_flg,
                                     :remove_tmp_eyecatch_flg,
+                                    :publish_flg,
                                     :update_type,
                                     tag_ids: [])
   end
@@ -78,7 +81,45 @@ class ArticlesController < ApplicationController
     params = article_params
     params[:tmp_title] = params[:title] if params[:title]
     params[:tmp_text] = params[:text] if params[:text]
-    params.slice('tmp_title', 'tmp_text', 'tmp_eyecatch')
+    params.slice(:tmp_title, :tmp_text, :tmp_eyecatch)
+  end
+
+  def publish_article_params
+    params = article_params
+    params[:publish_flg] = ActiveRecord::Type::Boolean.new.cast(params[:publish_flg])
+    params[:title] = params[:tmp_title] if params[:tmp_title]
+    params[:tmp_title] = nil
+    params[:text] = params[:tmp_text] if params[:tmp_text]
+    params[:tmp_text] = nil
+    if params[:tmp_eyecatch]
+      @article.remove_eyecatch!
+      @article.save
+      params[:eyecatch] = params[:tmp_eyecatch]
+      params[:tmp_eyecatch] = nil
+    end
+    # ↓いずれいらなくなるかも
+    params.except(:remove_eyecatch_flg, :remove_tmp_eyecatch_flg, :update_type)
+  end
+
+  def update_article(params)
+    if @article.update(params)
+      render json: {result: "ok"}
+    else
+      render json: {result: "error"}
+    end
+  end
+
+  def remove_eyecatch(tmp)
+    if tmp
+      @article.remove_tmp_eyecatch!
+    else
+      @article.remove_eyecatch!
+    end
+    if @article.save
+      render json: {result: "ok"}
+    else
+      render json: {result: "error"}
+    end
   end
 
 end
