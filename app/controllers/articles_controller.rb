cd07a7f4de92e731ff8d4ccd32c77880b09cfb1e
yutaro1031 <yutaro50@gmail.com>
2019-03-12 19:23:24 +0900

    記事検索機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,21 @@ class ArticlesController < ApplicationController
   before_action :set_target_article, only: %i[show edit update destroy]
   
   def index # 記事一覧を表示
-    if current_user && current_user.admin_flg
-      @articles = params[:tag_id].present? ? Tag.find(params[:tag_id]).articles : Article.where(del_flg: false)
+    find_params = {}
+    find_params[:del_flg] = false
+    find_params[:publish_flg] = true unless current_user && current_user.admin_flg
+
+    if params[:tag_id].present?
+      @list_title = "「#{Tag.find(params[:tag_id]).name}」の記事一覧"
+      @articles = Tag.find(params[:tag_id]).articles.where(find_params).order(created_at: "DESC")
     else
-      @articles = params[:tag_id].present? ? Tag.find(params[:tag_id]).articles : Article.where(del_flg: false, publish_flg: true)
+      if params[:q].present?
+        @list_title = "「#{params[:q]}」の検索結果"
+        @articles = Article.where(find_params).where("title LIKE ?", "%#{params[:q]}%").order(created_at: "DESC")
+      else
+        @list_title = "新着記事"
+        @articles = Article.where(find_params).order(created_at: "DESC")
+      end
     end
     @articles = @articles.page(params[:page])
     @popular_articles = Article.where(del_flg: false).order(:pv).limit(5)
