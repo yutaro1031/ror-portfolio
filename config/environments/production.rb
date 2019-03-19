8313234a75edce97dce2fd2d56ea70a904b0ac45
yutaro1031 <yutaro50@gmail.com>
2019-03-19 14:42:10 +0900

    heroku用に修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -96,4 +96,6 @@ Rails.application.configure do
   config.assets.precompile += Ckeditor.assets
   config.assets.precompile += %w( ckeditor/* )
   config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
+  config.assets.initialize_on_precompile=false
 end
