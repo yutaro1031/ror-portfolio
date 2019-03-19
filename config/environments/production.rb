8d34d5db878c21af20bcf7501c5c279e609c141d
yutaro1031 <yutaro50@gmail.com>
2019-03-19 20:40:03 +0900

    メーラーの設定調整3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
 
   # SendGrid
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options   = { host: 'ror-media-portfolio.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
